2d762dd777b1ee293dfffb72640b6fa338153a1e
Maxime Guy <maxime.guy@imt-atlantique.net>
2026-02-27 12:03:00 +0100

    Un peu d'opti du bot

--- chatbot.py
+++ chatbot.py
@@ -16,7 +16,6 @@ from langchain_openai import ChatOpenAI
 # Load environment variables
 load_dotenv()
 
-
 def main():
 
     model = ChatOpenAI(
@@ -25,17 +24,47 @@ def main():
         api_key=os.getenv("AI_API_KEY")
     )
 
-    # Start with system message and first question
-    messages = [
-        SystemMessage(content="Tu es un expert de la langue française et des champs lexicaux. Ton objectif est de trouver un mot un partir de tentatives. A chaque tentative, on te diras si tu es proches ou loin avec une température. Essaye des mots d'abords au hasard puis affine")
+    model2 = ChatOpenAI(
+        model=os.getenv("AI_MODEL"),
+        base_url=os.getenv("AI_ENDPOINT"),
+        api_key=os.getenv("AI_API_KEY")
+    )
+
+    message2 = [
+        SystemMessage(content="On va te donner des phrases qui contiennent chacune un mot à isoler, une proposition de réponse à un jeu. Tu ne dois renvoyer que ce mot.")
     ]
 
+    # Start with system message and first question
+    messages = [
+        SystemMessage(content="Tu es un expert de la langue française et des champs lexicaux. Ton objectif est de trouver un mot un partir de tentatives. A chaque tentative, on te diras si tu es proches ou loin avec une température (max 100°C, évolution logarithmique). Essaye des mots d'abords au hasard jusqu'à avoir plusieurs scores de plus de 50, puis affine. Tu commencera en proposant un premier mot. Il t'es interdit de dire plusieurs fois le même mot")
+    ]
+    init=input("Mots dont on dispose déjà : ")
+    init="Mots dont on connait déjà la température : " + init
+    messages.append(HumanMessage(content=init))
+    best_score=-100
+    best_world=""
     while messages[len(messages)-1].content != "100" : 
         response = model.invoke(messages)
         print(f"\n 🤓AI: {response.content}")
         messages.append(AIMessage(content=str(response.content)))
         message=input("score : ")
         messages.append(HumanMessage(content=message))
+        try:
+            if message=="inconnu":
+                messages.append(SystemMessage(content="Reste sur des mots non conjugués du dictionnaire Français."))
+            elif float(message)>best_score:
+                best_score=float(message)
+                message2.append(HumanMessage(content=response.content))
+                best_world=model2.invoke(message2).content
+                message2.remove(HumanMessage(content=response.content))
+            if best_score<15:
+                messages.append(SystemMessage(content="Change de champ lexical."))
+            elif float(message)<15:
+                messages.append(SystemMessage(content=f"Rapproche toi de {best_world}"))
+            messages.append(HumanMessage(content=f"Le meilleur mot est {best_world} avec {best_score}"))
+            print(f"Le meilleur mot est {best_world} avec {best_score}")
+        except ValueError:
+            print("Erreur de notation")
     print(len(messages))
 
     
